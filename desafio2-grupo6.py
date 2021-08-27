01ed6a5a756b98b5bf283bb891e9281de53334ca
Nikolas B Virionis <79850973+nikolas-virionis@users.noreply.github.com>
2021-08-26 22:32:57 -0300

    Update desafio2-grupo6.py

--- desafio2-grupo6.py
+++ desafio2-grupo6.py
@@ -4,61 +4,78 @@ import time
 
 def componente():
     while True:
-        print("\n\nOpções de visualização:\n\n\t0 - todos\n \t1 - cpu\n \t2 - maquina 2\n \t3 - maquina 3\n \t4 - maquinas 1 e 2\n \t5 - maquinas 1 e 3\n \t6 - maquinas 2 e 3\n")
+        print("\n\nOpções de visualização:\n\n\t0 - todos\n \t1 - cpu\n \t2 - ram\n \t3 - disco\n \t4 - internet\n")
         escolha = int(input("Escolha uma opção: "))
         if escolha == 0:
             return [1, 2, 3, 4]
-        if escolha in range(1, 4):
+        if escolha in range(1, 5):
             return [escolha]
         print("insira um valor válido")
 
 
 def metrics():
-    while True:
-        try:
-            times = int(
-                input("\nQuantas vezes deseja verificar as métricas de seu computador? "))
-            interval = int(
-                input("\nDe quantos em quantos segundos deseja medir essa métrica? "))
-            componentes = componente()
-            visualizacao(escolhas, times, interval)
-            break
-        except:
-            print(" Erro no numero inserido\n Tente novamente: \n\n")
+    componentes = componente()
+    visualizacao(componentes)
 
 
 def dados(escolhas):
-    maquina = {}
-    
-    maquina = {
-        "cpu": psutil.cpu_percent(interval=1),
-        "disco": psutil.disk_usage('/').percent,
-        "ram": psutil.virtual_memory().percent
+    maquina = []
+    componente1 = {
+        "nome": "CPU",
+        "nome1": "Porcentagem de CPU",
+        "nome2": "Frequencia de CPU",
+        "dado1": f"{psutil.cpu_percent(interval=1)}%",
+        "dado2": f"{psutil.cpu_freq().current}MHz"
     }
+    componente2 = {
+        "nome": "Memória Principal",
+        "nome1": "Quantidade de RAM livre",
+        "nome2": "Percentual de uso de RAM",
+        # 1048576 byte -> 1 Megabyte
+        "dado1": f"{round((psutil.virtual_memory().free / 1048576), 2)}MB",
+        "dado2": f"{psutil.virtual_memory().percent}%"
+    }
+    componente3 = {
+        "nome": "Memória em disco",
+        "nome1": "Quantidade de memoria em disco livre",
+        "nome2": "Percentual de memoria de disco em uso",
+        # 1073741824 byte -> 1 Gigabyte
+        "dado1": f"{round((psutil.disk_usage('/').free / 1073741824), 2)}GB",
+        "dado2": f"{psutil.disk_usage('/').percent}%"
+    }
+    componente4 = {
+        "nome": "Internet",
+        "nome1": "Bytes recebidos",
+        "nome2": "Bytes enviados",
+        # 1048576 byte -> 1 Megabyte
+        "dado1": f"{round((psutil.net_io_counters().bytes_recv / 1048576), 2)}MB",
+        # 1048576 byte -> 1 Megabyte
+        "dado2": f"{round((psutil.net_io_counters().bytes_sent / 1048576), 2)}MB"
+    }
+    for i in escolhas:
+        maquina.append(eval(f"componente{i}"))
     return maquina
 
 
-def visualizacao(escolhas, times, interval):
-    for i in range(times):
-        maquinas = dados(escolhas)
-        for maquina in maquinas:
-            for key, value in maquina.items():
-                if value >= 90:
-                    print(
-                        f"Máquina {maquina['maquina']} com excesso no uso de {key}. \n\t\t\tMANUTENÇÃO IMEDIATA")
+def visualizacao(escolhas):
+    while True:
+        maquina = dados(escolhas)
+        for componente in maquina:
             print(
-                f'\nMáquina {maquina["maquina"]}:\n Uso de CPU: {maquina["cpu"]:.2f}% \n Uso de Disco: {maquina["disco"]:.2f}% \n Uso de Memória: {maquina["ram"]:.2f}% \n\n')
+                f'\n{componente["nome"]}:\n\t{componente["nome1"]}: {componente["dado1"]}\n\t{componente["nome2"]}: {componente["dado2"]}')
+        if(1 in escolhas and psutil.cpu_freq().current*1.21 / psutil.cpu_freq().max > 1.2):
+            print(
+                "\n\n\tCPU rodando em overclock com frequência mais de 20% maior que o máximo, \n\t\t\t\t\tPERIGO")
         print("----------------------------------------------------------------------")
-        time.sleep(interval)
+        time.sleep(2)
 
 
-while True:
+def main():
     metrics()
-    print("\n\n0 - sair \n1 - repetir medições")
-    repeat = int(input("Escolha uma opção: "))
-    if not repeat:
-        break
 
+
+if __name__ == "__main__":
+    main()
 
 # Grupo 6 - SafeLog
 # Amanda         RA: 02211001
